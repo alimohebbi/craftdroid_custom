9da9232012a1b2581ef73e9f2b2e3839e79ddad6
ali mohebbi <ali.mohebbi@usi.ch>
2022-05-03 16:08:19 +0200

    redo disabled

--- run_manager.py
+++ run_manager.py
@@ -107,5 +107,5 @@ if __name__ == '__main__':
                                      'error': '',
                                      'test_exist': ''}
                         first_round_migration()
-                        redo_failed_migaratoins()
-                        results.to_csv(config.results, index=False)
+                        # redo_failed_migaratoins()
+                        results.dto_csv(config.results, index=False)
